d3f4f8c13a1c7b2a931fbb3729a267191bfb1790
sharifenterprises <163445780+sharifenterprises@users.noreply.github.com>
2024-03-14 10:00:18 -0700

    Sharif Enterprises

---
+++ .js
@@ -0,0 +1,225 @@
+
+body{
+  background-image: linear-gradient(-90deg, orange, yellow);
+
+background-size:cover;
+background-repeat:no-repeat;
+
+display:flex;
+flex-direction:column;
+min-height:100vh;
+margin:0;
+align-items:center;
+justify-content:center;
+}
+#content-wrapper{
+background:white;
+display:flex;
+flex-direction:column;
+min-height:100vh;
+width:60vw;
+margin:4em;
+padding:2.5em;
+}
+
+header{
+display:flex;
+flex-direction:column;
+align-items:center;
+font-size:60px;
+font-family: 'Montserrat', sans-serif;
+margin-bottom:60px;
+border-bottom:1px solid black;
+}
+
+ul{
+display:flex;
+flex-direction:row;
+justify-content:center;
+margin-bottom:60px;
+}
+ul li{
+display:inline;
+margin-right:40px;
+}
+a {
+text-decoration:none;
+color:black;
+}
+a:hover{
+color:green;
+}
+h1{
+color:rgb(216, 195, 59);
+}
+span{
+color:black;
+}
+p{
+text-align:justify;
+font-size:18px;
+}
+
+/*media queries*/
+
+@media screen and (max-width:1481px){
+ul li{
+font-size:13px;}
+}
+@media screen and (max-width:1311px){
+ul{
+  display:flex;
+  flex-direction:column;
+  margin-top: 80px;
+}
+ul li{
+  font-size:16px;
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<div id="content-wrapper">
+        <header>Benefits of Lemon Water</header>
+     <div class="ff">
+        <img src="https://images.unsplash.com/photo-1516148270512-7742a97f7676?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=41efb0c331c2aee0736bcc2f5e884ce1&auto=format&fit=crop&w=1350&q=80" height="570px">
+        </div>
+        <!--navigation-->
+        <nav>
+        <ul>
+          <li><a href="#hydration">Hydration</a></li>
+          <li><a href="#vitaminC">Vitamin C</a></li>
+          <li><a href="#skin">Skin</a></li>
+          <li><a href="#weightLoss">Weight Loss</a></li>
+          <li><a href="#digestion">Digestion</a></li>
+          <li><a href="#breath">Clean Breath</a></li>
+          <li><a href="#kidney">Prevents Kidney Stones</a></li>
+        </ul>
+        </nav>
+        
+        <!--content-->
+        <div id="hydration">
+          <h1><span>1.</span> It promotes hydration</h1>
+         <p> According to the Food and Nutrition Board, the dietary reference intake for water is 91 to 125 ounces. This includes water from food and drinks.</p>
+    
+    <p>Water is the best beverage for hydration, but some people don’t like the taste of it on its own. Adding lemon enhances water’s flavor, which may help you drink more.</p>
+    
+    
+        </div>
+        
+        <div id="vitaminC">
+          <h1><span>2.</span> It’s a good source of vitamin C</h1>
+          <p>Citrus fruits like lemons are high in vitamin C, which is a primary antioxidant that helps protect cells from damaging free radicals.</p>
+    
+    <p>Vitamin C may reduce your risk of cardiovascular disease and stroke, and lower blood pressure. Research published in Stroke showed that people with low vitamin C levels, especially obese men with high blood pressure, have a higher risk of stroke.</p>
+    
+    <p>Vitamin C may also help prevent or limit the duration of the common cold in some people, although studies are conflicting.</p>
+    
+    <p>While lemons don’t top the list of citrus fruits high in vitamin C, they’re still a good source. According to the United States Department of Agriculture, 1/4 cup raw lemon juice provides about 23.6 grams of vitamin C. That’s over 30 percent of the recommended daily allowance (RDA).</p>
+        </div>
+        
+        <div id="skin">
+          <h1><span>3.</span> It improves your skin quality</h1>
+          <p>Vitamin C found in lemons may help reduce skin wrinkling. A study published in the American Society for Clinical Nutrition concluded that people who consumed more vitamin C have less risk of wrinkled and dry skin.</p>
+    
+    <p>How water improves skin is controversial, but one thing is certain. If your skin loses moisture, it becomes dry and wrinkle-prone.</p>
+    
+    <p>Whether it’s better to apply moisturizer to the skin or drink more water isn’t clear, but UW Health recommends drinking at least eight glasses of water daily to stay hydrated and rid the skin of toxins.</p>
+     </div>
+        
+        <div id="weightLoss">
+          <h1><span>4.</span> It supports weight loss</h1>
+          <p>A study published in the Journal of Clinical Biochemistry and Nutrition showed that polyphenol antioxidants found in lemons significantly reduced weight gain caused by a high-fat diet in mice. In addition, insulin resistance was improved.</p>
+    
+    <p>While the same results need to be proven in humans, anecdotal evidence is strong that lemon water supports weight loss. Whether this is due to an increase in water intake and fullness or the lemons remains to be seen.</p>
+        </div>
+        
+        <div id="digestion">
+          <h1><span>5.</span> It aids digestion</h1>
+          <p>Some people drink lemon water as a daily morning laxative to help prevent constipation. Drinking warm or hot lemon water when you wake up may help get your digestive system moving.</p>
+    
+    <p>Ayurvedic medicine believes the sour lemon taste helps stimulate your “agni.” In Ayurveda, a strong agni jump-starts the digestive system, allowing you to digest food easily and helping prevent the buildup of toxins.</p>
+        </div>
+        
+        <div id="breath">
+          <h1><span>6.</span>It freshens breath</h1>
+          <p>Have you ever rubbed a lemon on your hands to remove a powerful stench? It’s thought to neutralize odors. The same folk remedy may apply to bad breath caused by eating foods with strong smells</p>
+    
+    <p>Keep your breath sweeter by drinking a glass of lemon water after meals and first thing in the morning. Lemon is thought to stimulate saliva, and water helps prevent a dry mouth, which leads to bad breath caused by excess bacteria growth.</p>
+        </div>
+        
+        <div id="kidney">
+          <h1><span>7.</span> It helps prevent kidney stones</h1>
+          <p>The citric acid in lemons may help prevent calcium kidney stones. UW Health recommends increasing citric acid intake to decrease your risk of getting new calcium stones. Drinking lemon water not only helps you get more citric acid, but also the water you need to prevent stones.</p>
+    
+    <p>Having 1/2 cup of lemon juice provides the same amount of citric acid you’d find in prescription varieties.</p>
+        </div>
+        
+        <br>
+        <center><a href="#top">Back To Top</a></center>
+      </div>
+      
+
